94f8b8b314f248c2923160a1e7b6537e5894b34d
Nicholas Ilacqua <Niservices88@gmail.com>
2021-11-13 14:18:28 -0600

    trying to do a function to make my buttons work

--- Server/public/client.js
+++ Server/public/client.js
@@ -2,25 +2,49 @@ $(document).ready(onReady);
 
 function onReady (){
     $('#equals').on('click', postAnswer)
-    
+    $('#addition').on('click', addOnClick)
+
 }
+let mathSymbol;
+function addOnClick (){
+    mathSymbol = '+'
+}
+
 
 function postAnswer (){
 // console.log('postAnswer is working');
 let mathEquationSend = {
-    numberOne: $('#numberOne').val(), 
-    numberTwo: $('#numberTwo').val()
-}
-    
-}$.ajax({
+    numberOne: $('#numberOne').val(),
+    mathSymbol: mathSymbol,
+    numberTwo: $('#numberTwo').val(),
+} 
+$.ajax({
     method: 'POST', 
     url: '/answers', 
     data: mathEquationSend
 }).then (function(response){
     $('#numberOne').val(''),
     $('#numberTwo').val('')
-    
+    getCalcutation()
 }).catch (function (error){
     console.log('not working');
     
 })
+}
+
+function getCalcutation (){
+    $.ajax({
+        method:'GET', 
+        url: '/answers'
+    }).then(function(response){
+        let mathProblemsel = $('#mathProblems')
+        mathProblemsel.empty();
+        for(let solution of response){
+            mathProblemsel.append(`
+            <li>${solution.numberOne} 
+                ${solution.mathSymbol}
+                ${solution.numberTwo}
+            </li>
+              `)
+        }
+})}
--- Server/server.js
+++ Server/server.js
@@ -5,7 +5,6 @@ const express = require ('express');
 const app = express(); 
 const PORT = 5000;
 const mathProblems = [];
-const mathCalculations = [];
 
 
 app.use(express.static('./server/public'));
@@ -14,15 +13,32 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/answers', (rep,res) =>{
     console.log('in the get/anwsers');
-    
+    res.send(mathProblems);
+     calculations();
 })
+
+function calculations (calculation){
+for (let number of calculation){
+  if (number.mathSymbol === '+'){
+     Number(number.numberOne + number.numberTwo)
+ }else if (number.mathSymbol=== '-') {
+     return Number(number.numberOne - number.numberTwo)
+  } else if (number.mathSymbol === '*'){
+     return Number(number.numberOne * number.numberTwo)
+  } else if (number.mathSymbol === '/') {
+     return Number(number.numberOne / number.numberTwo)
+ }
+}
+
+}
 
 app.post('/answers', (req,res) =>{
-    console.log('in the post/answers');
-    
+    console.log('in the post/answers', req.body);
+    mathProblems.push(req.body);
+    res.sendStatus(200);
 })
-app.listen(PORT, function () {
 
-    
+
+app.listen(PORT, function () {
     })
 
